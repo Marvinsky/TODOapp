ab8b8a821304d2b059519ed46a07cc356336860d
Marvinsky <nivramsky@gmail.com>
2017-02-04 09:07:53 -0500

    add toolbar for detail

--- app/src/main/java/todo/mobile/com/todoapp/details/TaskDetailsActivity.java
+++ app/src/main/java/todo/mobile/com/todoapp/details/TaskDetailsActivity.java
@@ -2,6 +2,8 @@ package todo.mobile.com.todoapp.details;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 import todo.mobile.com.todoapp.R;
 
@@ -11,5 +13,25 @@ public class TaskDetailsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_task_details);
+        showToolbar(getResources().getString(R.string.toolbar_title_detail), true);
+    }
+
+
+    public void showToolbar(String title, boolean upButton) {
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle(title);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(upButton);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        switch (id) {
+            case android.R.id.home:
+                onBackPressed();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
